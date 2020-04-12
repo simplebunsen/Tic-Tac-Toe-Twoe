619e4fb2437587bb356922fbe5b9b5dc3fe583ac
simplebunsen <really@dontdoxme.com>
2020-04-12 16:16:57 +0200

    Made the code accept a simple input to build a playing field with (unvalidated) chars.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,36 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter cells: ");
+        String input = scanner.next();
+
+        printHorLine();
+        for (int row = 1; row <= 3; row++) {
+            for (int col = 1; col <= 5; col++) {
+                switch (col) {
+                    case 1:
+                    case 5:
+                        System.out.print("| ");
+                        break;
+                    default:
+                        char c = input.charAt(0);
+                        System.out.print(c + " ");
+                        input = input.substring(1);
+                        //System.out.println("string is now " + input);
+                        break;
+                }
+            }
+            System.out.println();
+        }
+        printHorLine();
+    }
+
+    public static void printHorLine(){
+        System.out.println("---------");
     }
 }
